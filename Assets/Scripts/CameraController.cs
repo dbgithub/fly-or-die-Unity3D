2abfe8da826e6c3d71e822286489a3938a6dc6ef
Aitor DB <aitor.db@opendeusto.es>
2017-05-14 20:50:09 +0200

    making the helicopter fly with a controller. A camera controller was created too, but it didn't work as expected.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject targetObject;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = targetObject.transform.TransformPoint (0,4,5);
+		transform.position = Vector3.Lerp (transform.position, offset, 7 * Time.deltaTime);
+		transform.LookAt (targetObject.transform.position);
+	}
+}
--- Assets/Scripts/HelicopterController.cs
+++ Assets/Scripts/HelicopterController.cs
@@ -9,47 +9,68 @@ public class HelicopterController : MonoBehaviour {
 	private Rigidbody rb;
 
 	// Main rotor
-	public float maxRotorForce;
-	public float maxRotorVelocity;
-	static float rotorVelocity;
-	private float rotorRotation;
-	public bool mainRotorActive;
+	public float maxRotorForce = 24000f;
+	public float maxRotorVelocity = 7200f;
+	static float rotorVelocity = 0f;
+	private float rotorRotation = 0f;
+	public bool mainRotorActive = true;
 	// Tail rotor
-	public float maxTailRotorForce;
-	public float maxTailRotorVelocity;
-	private float tailRotorVelocity;
-	private float tailRotorRotation;
-	public bool mainTailRotorActive;
+	public float maxTailRotorForce = 25000f;
+	public float maxTailRotorVelocity = 2200f;
+	private float tailRotorVelocity = 0f;
+	private float tailRotorRotation = 0f;
+	public bool tailRotorActive = true;
 	// Helicopter torque
-	public float forwardRotorTorqueMultiplier;
-	public float sidewaysRotorTorqueMultiplier;
+	public float forwardRotorTorqueMultiplier = 0.5f;
+	public float sidewaysRotorTorqueMultiplier = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-		maxRotorForce = 24000f;
-		maxRotorVelocity = 7200f;
-		rotorVelocity = 0f;
-		rotorRotation = 0f;
-		mainRotorActive = true;
-		maxTailRotorForce = 25000f;
-		maxTailRotorVelocity = 2200f;
-		tailRotorVelocity = 0f;
-		tailRotorRotation = 0f;
-		mainTailRotorActive = true;
-		forwardRotorTorqueMultiplier = 0.5f;
-		sidewaysRotorTorqueMultiplier = 0.5f;
-
-		rb = GetComponent (Rigidbody);
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 torqueValue;
-		Vector3 controlTorque = new Vector3(Input.GetAxis("Vertical")*forwardRotorTorqueMultiplier, 1.0, -Input.GetAxis("Horizontal2") * sidewaysRotorTorqueMultiplier);
+		Vector3 torqueValue = new Vector3();
+		Vector3 controlTorque = new Vector3(Input.GetAxis("Vertical")*forwardRotorTorqueMultiplier, 1.0f, -1 * Input.GetAxis("Horizontal2") * sidewaysRotorTorqueMultiplier);
+		if (mainRotorActive == true) {
+			torqueValue += (controlTorque * maxRotorForce * rotorVelocity);
+			rb.AddRelativeForce (Vector3.up * maxRotorForce * rotorVelocity);
+		}
+		if (Vector3.Angle(Vector3.up, transform.up) < 80) {
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y,0), Time.deltaTime * rotorVelocity * 2);
+		}
+		if (tailRotorActive == true) {
+			torqueValue -= (Vector3.up * maxTailRotorForce * tailRotorVelocity);
+			rb.AddRelativeTorque (torqueValue);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (mainRotorActive == true) {
+			mainRotor.transform.rotation = transform.rotation * Quaternion.Euler (270, rotorRotation, 0);
+		}
+		if (tailRotorActive == true) {
+			tailRotor.transform.rotation = transform.rotation * Quaternion.Euler (0, 270, tailRotorRotation);
+		}
+		rotorRotation += maxRotorVelocity * rotorVelocity * Time.deltaTime;
+		tailRotorRotation += maxTailRotorVelocity * rotorVelocity * Time.deltaTime;
+
+		float hoverRotorVelocity = (rb.mass * Mathf.Abs (Physics.gravity.y) / maxRotorForce);
+		float hoverTailRotorVelocity = (maxRotorForce * rotorVelocity) / maxTailRotorForce;
+
+		if (Input.GetAxis ("Vertical2") != 0.0f) {
+			rotorVelocity += Input.GetAxis ("Vertical2") * 0.005f;
+		} else {
+			rotorVelocity = Mathf.Lerp (rotorVelocity, hoverRotorVelocity, Time.deltaTime * 5);
+		}
+		tailRotorVelocity = hoverTailRotorVelocity - Input.GetAxis ("Horizontal") * 0.4f;
+
+		if (rotorVelocity > 1.0) {
+			rotorVelocity = 1.0f;
+		} else if (rotorVelocity < 0.0) {
+			rotorVelocity = 0.0f;
+		}
 	}
 }
